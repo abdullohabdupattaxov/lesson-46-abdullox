683906e5e5237f9561740c042fc40cddec2d9656
abdullohabdupattaxov <abdulloxsteam928@gmail.com>
2025-07-15 13:49:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log(852655)
+console.log("abdullox")
+
+var name = "Alex"
+let age = 25;
+const PI = 3.14;
+
+
+console.log("первая строка вторая строка ")
+let user = prompt("Ismingiz nima?")
+let  familiya = prompt("Familiyangiz nima?")
+alert("Salom, " + user + " " + familiya)
+let yashashJoy = prompt("Qayerda yashaysiz?")
+alert("Siz " + yashashJoy + " da yashaysiz")
+let fan = prompt("Qaysi fanga qiziqasiz?")
+alert("Sizga " + fan + " fani yoqadi")
+let ustoz = prompt("Ustozingiz kim?")
+alert("Ustozingiz: " + ustoz)
+let oquvJoy = prompt("Qayerda o'qiysiz?")
+alert("Siz " + oquvJoy + " da o'qiysiz")
